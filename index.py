6cc23d8b7c1949782946941cffa6f28b97e4d9ec
DEV Sayan 0007 <sayan.c.development@gmail.com>
2021-11-01 22:09:36 +0530

    Add Datatype

--- index.py
+++ index.py
@@ -11,3 +11,40 @@ print(a,'a')
 c = "hello"
 
 print(c)
+
+# Data Types in python
+# """ 
+#  1) number
+#    i) Integers
+#    ii)Float
+#    iii) Complex Number
+#  2) Sequence Type
+#    i) String
+#    ii) List
+#    iii) Tuple
+#  3) Dictionary
+#  4) Set
+ 
+#  Mutable Data Type
+#   i) List
+#   ii) Dictionaty
+#   iii) byte array
+ 
+#  Immutable Data Type
+#   i) int
+#   ii) Float
+#   iii) Complex
+#   iv) String
+#   v) Tuple
+#   vi) Set
+ 
+# """
+
+a = 10 
+b = 10.2
+c = 1+2j
+d = "hello"
+print(a," is of type ",type(a))
+print(b," is of type ",type(b))
+print(c," is of type ",type(c))
+print(d," is of type ",type(d))
