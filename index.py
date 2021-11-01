ca0b132515f9f534c69c0cf7900986a0191e4b5f
DEV Sayan 0007 <sayan.c.development@gmail.com>
2021-11-01 22:30:06 +0530

    2nd commit

--- index.py
+++ index.py
@@ -4,3 +4,17 @@
 # ii) int()
 # iii) float()
 # iv) eval()
+
+a = input("Enter The Value ")
+print(a," ", type(a))
+
+b = int(input("Enter an integer value "))
+print(b," ", type(b))
+
+c = float(input("Enter a Float value "))
+
+print(c," ", type(c))
+
+d = eval(input("Enter anything "))
+
+print(d," ", type(d))
