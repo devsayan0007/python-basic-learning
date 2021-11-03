3fd187b79bb19ecb116cec4ee82d441b04a4fcb4
DEV Sayan 0007 <sayan.c.development@gmail.com>
2021-11-03 13:46:01 +0530

    2nd commit

--- index.py
+++ index.py
@@ -5,6 +5,16 @@ print('''
       * Multiply
       / Divide''')
 num1 = float(input("Enter 1st Value "))
-opr = input("Enter the Operator ")
 num2 = float(input("Enter 2nd Value "))
+opr = input("Enter the Operator ")
 
+if opr=="+":
+  print(num1+num2)
+elif opr=="-":
+  print(num1-num2)
+elif opr=="*":
+  print(num1*num2)
+elif opr=="/":
+  print(num1/num2)
+else:
+  print("Invalid Option")
