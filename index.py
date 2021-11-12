4d97c428ae3f172bfa5b4c4633d7d678717cf82a
DEV Sayan 0007 <sayan.c.development@gmail.com>
2021-11-12 19:45:15 +0530

    create datadase

--- index.py
+++ index.py
@@ -1,15 +1,14 @@
-#start
-#condition
-#Increment/Decrement
-
-i = 1
-while i<=10:
-    print("welcome")
-    i=i+1
-
-a = 10
-while a>=1:
-    print(a,"hello")
-    a=a-1
-
-print(a)
+import sqlite3
+con = sqlite3.Connection("./database/sqlite3.db")
+try:
+    con.execute('''
+               CREATE TABLE student (
+                 st_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                 st_name VARCHAR(50),
+                 st_class VACHAR(10),
+                 st_email VARCHAR(30)
+               )
+    ''')
+    print("CREATE TABLE SUCCESSFULLY")
+except:
+     print("ERROR TO CREATE TABLE")
